67052253351ca3d87981440361f93d8e6332881f
plumberg <hana.ryzhuk@gmail.com>
2019-02-07 20:53:37 -0500

    basics created, no logic yes

---
+++ Console_Asgn1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Asgn1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            Int32 num;
+
+            Console.WriteLine("Hello!");
+            do
+            {
+                Console.WriteLine("Are you natural Born Citizen? Y - for yes, N - for No");
+                input = Console.ReadLine();
+                /* if (input.Equals("Y", StringComparison.InvariantCultureIgnoreCase))
+                 {
+                     Console.WriteLine("Y was pressed");
+                     Console.ReadKey(true);
+                 }*/
+            } while (!input.Equals("Y", StringComparison.InvariantCultureIgnoreCase));
+
+            do
+            {
+                Console.WriteLine("How old are you?");
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num < 35)
+                {
+                    Console.WriteLine("Should be at least 35 years old");
+                }
+            } while (num < 35);
+
+            do
+            {
+                Console.WriteLine("How many years have you resided n the US?");
+                num = Convert.ToInt32(Console.ReadLine());
+
+            } while (num < 14);
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ GUI_Asgn_1/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace GUI_Asgn_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBoxResided = new System.Windows.Forms.TextBox();
+            this.textBoxTerms = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(52, 31);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(119, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Natural Born Citizen";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Location = new System.Drawing.Point(308, 77);
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(36, 20);
+            this.textBoxAge.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "How old are you?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "How many years have you resided in the US?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(196, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "How many prior terms have you served?";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(52, 207);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(169, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "I have rebelled against the US";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // textBoxResided
+            // 
+            this.textBoxResided.Location = new System.Drawing.Point(308, 122);
+            this.textBoxResided.Name = "textBoxResided";
+            this.textBoxResided.Size = new System.Drawing.Size(36, 20);
+            this.textBoxResided.TabIndex = 6;
+            // 
+            // textBoxTerms
+            // 
+            this.textBoxTerms.Location = new System.Drawing.Point(308, 164);
+            this.textBoxTerms.Name = "textBoxTerms";
+            this.textBoxTerms.Size = new System.Drawing.Size(36, 20);
+            this.textBoxTerms.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(170, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 38);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Determine Eligibility";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 302);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxTerms);
+            this.Controls.Add(this.textBoxResided);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxAge);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "US Presidential Eligibility";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBoxResided;
+        private System.Windows.Forms.TextBox textBoxTerms;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
