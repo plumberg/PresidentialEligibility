fc5e7a13432a1ee713f1cf058ffd06b23e98f11a
plumberg <hana.ryzhuk@gmail.com>
2019-02-10 16:38:27 -0500

    core done, console works

--- Asgn_01_Lib/Logic.cs
+++ Asgn_01_Lib/Logic.cs
@@ -8,5 +8,33 @@ namespace Asgn_01_Lib
 {
     public class Logic
     {
+        Boolean citizen;
+        Int32 resided;
+        Int32 age;
+        Int32 terms;
+        Boolean rebelled;
+
+        public Logic(Boolean citizen,
+         Int32 resided,
+         Int32 age,
+         Int32 terms,
+         Boolean rebelled)
+        {
+            this.citizen = citizen;
+            this.resided = resided;
+            this.age = age;
+            this.terms = terms;
+            this.rebelled = rebelled;
+        }
+
+        public Boolean CheckElligibility()
+        {
+            if (citizen == false || resided < 14
+                || age < 35 || terms >= 2 || rebelled == true)
+            {
+                return false;
+            }
+            else return true;
+        }
     }
 }
--- Console_Asgn1/Program.cs
+++ Console_Asgn1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Asgn_01_Lib;
 
 namespace Console_Asgn1
 {
@@ -11,37 +12,75 @@ namespace Console_Asgn1
         static void Main(string[] args)
         {
             string input = "";
-            Int32 num;
+            Int32 numTerms, numAge, numResided;
+            Boolean rebelled = false;
+            Boolean citizen = false;
+            Boolean check;
 
             Console.WriteLine("Hello!");
-            do
-            {
+            
                 Console.WriteLine("Are you natural Born Citizen? Y - for yes, N - for No");
                 input = Console.ReadLine();
-                /* if (input.Equals("Y", StringComparison.InvariantCultureIgnoreCase))
+                 if (input.Equals("Y", StringComparison.InvariantCultureIgnoreCase))
                  {
-                     Console.WriteLine("Y was pressed");
-                     Console.ReadKey(true);
-                 }*/
-            } while (!input.Equals("Y", StringComparison.InvariantCultureIgnoreCase));
-
-            do
-            {
-                Console.WriteLine("How old are you?");
-                num = Convert.ToInt32(Console.ReadLine());
-                if (num < 35)
+                     //Console.WriteLine("Y was pressed");
+                     citizen = true;
+                    
+                 }else if (input.Equals("N", StringComparison.InvariantCultureIgnoreCase))
                 {
-                    Console.WriteLine("Should be at least 35 years old");
+                    //Console.ReadKey(true);
+                    citizen = false;
                 }
-            } while (num < 35);
+     
 
-            do
+           
+
+                Console.WriteLine("How old are you?");
+                numAge = Convert.ToInt32(Console.ReadLine());
+               
+            
+                Console.WriteLine("How many years have you resided in the US?");
+                numResided = Convert.ToInt32(Console.ReadLine());
+            
+           
+                Console.WriteLine("How many prior terms you've served?");
+                numTerms = Convert.ToInt32(Console.ReadLine());
+               
+
+                Console.WriteLine("Did you rebell against US? Y for yes N for No");
+                input = Console.ReadLine();
+         
+                
+                if (input.Equals("Y", StringComparison.InvariantCultureIgnoreCase))
+                {
+                //Console.WriteLine("Y was pressed");
+                rebelled = true;
+
+                }
+                else if (input.Equals("N", StringComparison.InvariantCultureIgnoreCase))
+                {
+                //Console.ReadKey(true);
+                rebelled = false;
+                }
+
+
+
+            Logic logic = new Logic(citizen,numResided,numAge,numTerms,rebelled);
+            check = logic.CheckElligibility();
+            if (check==true)
             {
-                Console.WriteLine("How many years have you resided n the US?");
-                num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("You are eligible for the Presidential Election");
+                Console.ReadKey(true);
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you are not eligible");
+                Console.ReadKey(true);
+            }
 
-            } while (num < 14);
-            Console.ReadKey(true);
+
         }
+
+        
     }
 }
